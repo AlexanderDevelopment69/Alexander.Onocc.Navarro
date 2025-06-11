b3cdb294afb856ea8e25c2c9181beeec3ecac2e7
Alexander O.N <bryano_98@hotmail.com>
2025-06-11 10:55:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ btnToggle.addEventListener('click', () => {
   sidebar.classList.toggle('collapsed');
 });
 
-// Cambiar clase active en links
+// Cambiar clase active en links y cerrar sidebar en móvil
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
     navLinks.forEach(l => l.classList.remove('active'));
     link.classList.add('active');
+
+    if(window.innerWidth <= 900) {
+      sidebar.classList.add('collapsed');
+    }
   });
 });
